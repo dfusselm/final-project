03fed99df7833b161bbb82aebb9c876fca76d7b7
dfusselm <165109847+dfusselm@users.noreply.github.com>
2024-05-04 15:50:12 +0000

    added teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Team resource:
+
+  # CREATE
+  post("/insert_team", { :controller => "teams", :action => "create" })
+          
+  # READ
+  get("/teams", { :controller => "teams", :action => "index" })
+  
+  get("/teams/:path_id", { :controller => "teams", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_team/:path_id", { :controller => "teams", :action => "update" })
+  
+  # DELETE
+  get("/delete_team/:path_id", { :controller => "teams", :action => "destroy" })
+
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
